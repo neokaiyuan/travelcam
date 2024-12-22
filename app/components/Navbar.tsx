24f28f75981e74c4624f236d25d61bf05da367b2
Neo Kai Yuan <3345016+neokaiyuan@users.noreply.github.com>
2024-12-22 13:36:37 +0800

    Use standard icon instead of custom icon in nvabar for simplicity for now

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { FaCamera } from "react-icons/fa";
+import { FaCamera, FaBars } from "react-icons/fa";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-icon">
-        <div className="icon-bar long"></div>
-        <div className="icon-bar short"></div>
+        <FaBars size={24} color="white" />
       </div>
       <div className="navbar-title">Wandercam</div>
       <div className="navbar-camera-icon">
