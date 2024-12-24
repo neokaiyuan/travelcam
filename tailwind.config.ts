e5dbe5e00daded2002ff348db1ac6a5af81ace7e
Neo Kai Yuan <3345016+neokaiyuan@users.noreply.github.com>
2024-12-25 00:32:56 +0800

    App will always be in dark mode. Refactored colours into variables to facilitate future light mode.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--root-background-color)",
+        foreground: "var(--root-foreground-color)",
       },
     },
   },
